10b799cbf498864ae4ee62a6144d7344e4e74a33
josdem <joseluis.delacruz@gmail.com>
2021-05-28 09:15:17 -0400

    Updating Java and Gradle versions

---
+++ src/main/java/com/jos/dem/springboot/thymeleaf/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.jos.dem.springboot.thymeleaf.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class DemoController{
+
+  @RequestMapping("/")
+  public String index(){
+    return "Hello World!";
+  }
+
+}
